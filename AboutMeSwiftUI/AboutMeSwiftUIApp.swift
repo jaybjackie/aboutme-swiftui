4e3f932496cd85557e743bf6ab9445ab829abb30
jaybjackie <kodchakan.forwork@gmail.com>
2023-03-16 09:31:51 +0700

    Initial Commit

---
+++ AboutMeSwiftUI/AboutMeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeSwiftUIApp.swift
+//  AboutMeSwiftUI
+//
+//  Created by Jay B on 16/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMeSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AboutMeSwiftUI
+//
+//  Created by Jay B on 16/3/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
